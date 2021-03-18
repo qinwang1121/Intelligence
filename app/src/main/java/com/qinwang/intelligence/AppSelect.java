810623dc5e2b88acba6bca5135ec4ecae3a61d0c
qinwang <haoyanwang1121@gmail.com>
2021-03-18 21:38:39 +0800

    添加App选择判断，

---
+++ app/src/main/java/com/qinwang/intelligence/AppSelect.java
@@ -0,0 +1,25 @@
+package com.qinwang.intelligence;
+
+/**
+ * @Auther:haoyanwang1121@gmail.com
+ * @Date:2021/3/18
+ * @Description:com.qinwang.intelligence
+ * @Version:1.0
+ * @function:
+ */
+public class AppSelect {
+    private String appUser = null;
+
+    public String getAppUser() {
+        if (BuildConfig.PURPOSE.equals("admin")){
+            appUser = "admin";
+        }else if (BuildConfig.PURPOSE.equals("user")){
+            appUser = "user";
+        }
+        return appUser;
+    }
+
+    public void setAppUser(String appUser) {
+        this.appUser = appUser;
+    }
+}
