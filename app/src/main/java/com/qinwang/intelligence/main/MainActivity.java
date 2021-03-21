43de289363d3b1deacd7389fbe5f1e11b4a7b151
qinwang <haoyanwang1121@gmail.com>
2021-03-21 16:43:55 +0800

    根据不同的用户登录不同的APP，显示不同的主界面

--- app/src/main/java/com/qinwang/intelligence/main/MainActivity.java
+++ app/src/main/java/com/qinwang/intelligence/main/MainActivity.java
@@ -1,16 +1,41 @@
 package com.qinwang.intelligence.main;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.qinwang.base.BaseActivity;
+import com.qinwang.intelligence.BuildConfig;
 import com.qinwang.intelligence.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,
+                R.id.fragment);
+        if (BuildConfig.PURPOSE.equals("admin")){
+            bottomNavigationView.inflateMenu(R.menu.menu_admin);
+            navController.setGraph(R.navigation.admin_navigation);
+        }else if (BuildConfig.PURPOSE.equals("user")){
+            bottomNavigationView.inflateMenu(R.menu.menu_user);
+            navController.setGraph(R.navigation.user_navigation);
+        }
+        AppBarConfiguration configuration = new AppBarConfiguration
+                .Builder(bottomNavigationView.getMenu())
+                .build();
+        NavigationUI.setupActionBarWithNavController(this,
+                navController,
+                configuration);
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                navController);
     }
 }
