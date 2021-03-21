14dc392c01543786c3aacebbec6581c7cda4abac
qinwang <haoyanwang1121@gmail.com>
2021-03-21 20:25:46 +0800

    账号管理界面完成布局

--- app/src/main/java/com/qinwang/intelligence/fragment/myself/safety/SafetyActivity.java
+++ app/src/main/java/com/qinwang/intelligence/fragment/myself/safety/SafetyActivity.java
@@ -1,15 +1,28 @@
 package com.qinwang.intelligence.fragment.myself.safety;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 import com.qinwang.base.BaseActivity;
 import com.qinwang.intelligence.R;
 
 public class SafetyActivity extends BaseActivity {
 
+    TextView toolbar_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety);
+
+        initView();
+    }
+
+    public void initView(){
+        toolbar_title = (TextView)findViewById(R.id.toolber_titlle);
+
+        toolbar_title.setText(getString(R.string.safetyTitle));
+        toolbar_title.setGravity(Gravity.CENTER);
     }
 }
