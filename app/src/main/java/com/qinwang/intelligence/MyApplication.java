f444fbc55889ade32838ae3b0ed70ffac6601791
qinwang <haoyanwang1121@gmail.com>
2021-03-30 19:23:46 +0800

    地图显示

--- app/src/main/java/com/qinwang/intelligence/MyApplication.java
+++ app/src/main/java/com/qinwang/intelligence/MyApplication.java
@@ -2,6 +2,9 @@ package com.qinwang.intelligence;
 
 import android.app.Application;
 
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+
 /**
  * @Auther:haoyanwang1121@gmail.com
  * @Date:2021/3/30
@@ -12,4 +15,13 @@ import android.app.Application;
 public class MyApplication extends Application {
     public static boolean isFirstLogin = true;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        SDKInitializer.initialize(this);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+    }
 }
--- app/src/main/java/com/qinwang/intelligence/main/fragment/home/HomeFragment.java
+++ app/src/main/java/com/qinwang/intelligence/main/fragment/home/HomeFragment.java
@@ -8,10 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
 import com.qinwang.base.BaseFragment;
 import com.qinwang.intelligence.R;
 
 public class HomeFragment extends BaseFragment {
+
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
 
     @Nullable
     @Override
@@ -22,5 +30,26 @@ public class HomeFragment extends BaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        initView();
+        setMap();
+    }
+
+    public void initView(){
+        mMapView = getView().findViewById(R.id.bMapView);
+    }
+
+    public void setMap(){
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        MapStatus mMapStatus = new MapStatus
+                .Builder()
+                .zoom(20)                                               //设置级别，放大地图到18倍
+                .build();
+        //定义MapStatusUpdate对象，以便描述地图状态将要发生的变化
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+        //改变地图状态
+        mBaiduMap.setMapStatus(mMapStatusUpdate);
+        mBaiduMap.setBuildingsEnabled(true);
     }
 }
