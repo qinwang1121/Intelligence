ba7e0b933bcd4a61013c5564b75cf202740d019b
qinwang <haoyanwang1121@gmail.com>
2021-05-20 23:10:18 +0800

    1、修复意见反馈时反馈意见和邮箱为空时就可上传的bug 2、信息管理中按键添加事件

--- app/src/main/java/com/qinwang/intelligence/main/fragment/myself/safety/SafetyActivity.java
+++ app/src/main/java/com/qinwang/intelligence/main/fragment/myself/safety/SafetyActivity.java
@@ -4,12 +4,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.qinwang.base.BaseActivity;
 import com.qinwang.intelligence.R;
 
-public class SafetyActivity extends BaseActivity {
+public class SafetyActivity extends BaseActivity implements View.OnClickListener {
 
     private TextView toolbar_title, safetyUserName, safetyUserSex, safetyUserID,
             safetyPassWord, safetyTel, safetyHome;
@@ -43,5 +44,14 @@ public class SafetyActivity extends BaseActivity {
         safetyPassWord.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         safetyTel.setText(sharedPreferences.getString("phone", null));
         safetyHome.setText(sharedPreferences.getString("home", null));
+
+        findViewById(R.id.makeSure).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.makeSure){
+            finish();
+        }
     }
 }
